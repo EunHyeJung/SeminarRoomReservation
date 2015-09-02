3d02925c3f990e7fdbf732ff82fe26225d0ef47c
JungEunHye <zxy826@naver.com>
2015-09-02 12:26:07 +0900

    first commit

---
+++ Capstone2_Project/SeminarRoomReservation/app/src/main/java/kr/ac/kookmin/cs/capstone2/seminarroomreservation/ReservationStatusFragment.java
@@ -0,0 +1,30 @@
+package kr.ac.kookmin.cs.capstone2.seminarroomreservation;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ReservationStatusFragment extends Fragment {
+
+
+    public ReservationStatusFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_reservation_status, container, false);
+    }
+
+
+}
