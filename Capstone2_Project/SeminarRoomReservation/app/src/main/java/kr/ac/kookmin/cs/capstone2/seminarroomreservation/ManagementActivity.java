96ebd6db0bcd7332d37803e72e6a876c9b6b2dca
SongMiLee <s91010@naver.com>
2015-09-09 12:38:21 +0900

    add the Seminar Room ListView layout

--- Capstone2_Project/SeminarRoomReservation/app/src/main/java/kr/ac/kookmin/cs/capstone2/seminarroomreservation/ManagementActivity.java
+++ Capstone2_Project/SeminarRoomReservation/app/src/main/java/kr/ac/kookmin/cs/capstone2/seminarroomreservation/ManagementActivity.java
@@ -4,14 +4,34 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 
-public class ManagementActivity extends ActionBarActivity {
+public class ManagementActivity extends ActionBarActivity{
+    ListView SeminarListView;
+    ListViewAdapter SeminarListViewAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_management);
+
+        //Create Custom Adapter
+        SeminarListViewAdapter=new ListViewAdapter();
+
+        //Mapping the ListView
+        SeminarListView=(ListView)findViewById(R.id.SeminarList);
+
+        SeminarListView.setAdapter(SeminarListViewAdapter);
+
+        //Add the item to listview
+        SeminarListViewAdapter.add("Seminar Room A");
+        SeminarListViewAdapter.add("Seminar Room B");
+        SeminarListViewAdapter.add("Seminar Room C");
+        SeminarListViewAdapter.add("Seminar Room D");
+        SeminarListViewAdapter.add("Seminar Room E");
+        SeminarListViewAdapter.add("Seminar Room F");
+        SeminarListViewAdapter.add("Seminar Room G");
     }
 
 
@@ -36,4 +56,5 @@ public class ManagementActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
