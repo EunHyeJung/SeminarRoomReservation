f8b7238d0658cf3d179e8c20aa5b0966f23222cb
JuhaeHong <juhae9117@gmail.com>
2015-09-17 17:45:28 +0900

    Reservation form layout

--- Capstone2_Project/SeminarRoomReservation/app/src/main/java/kr/ac/kookmin/cs/capstone2/seminarroomreservation/ReservationFormActivity.java
+++ Capstone2_Project/SeminarRoomReservation/app/src/main/java/kr/ac/kookmin/cs/capstone2/seminarroomreservation/ReservationFormActivity.java
@@ -7,42 +7,93 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
-//150909
 //jh
 //예약신청 페이지
-public class ReservationFormActivity extends AppCompatActivity {
+public class ReservationFormActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-    private EditText check_in_date = (EditText) findViewById(R.id.check_in_date);
-    private EditText check_in_time = (EditText) findViewById(R.id.check_in_time);
-    private EditText check_out_time = (EditText) findViewById(R.id.check_out_time);
-    private EditText useTime = (EditText) findViewById(R.id.useTime);
-    private EditText roomNumber = (EditText) findViewById(R.id.roomNumber);
-    private EditText reason = (EditText) findViewById(R.id.reason);
-    private EditText otherMembers = (EditText) findViewById(R.id.otherMembers);
-    private Button button_makereservation = (Button) findViewById(R.id.button_makereservation);
+    private EditText check_in_date;
+    private EditText check_in_time;
+    private EditText check_out_time;
+    private EditText useTime;
+    private EditText roomNumber;
+    private EditText reason;
+    private EditText otherMembers;
+    private Button button_makereservation;
 
     int year, month, day, hour, minute;
 
+    Spinner spinner;
+    ArrayAdapter<CharSequence> adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reservationform);
 
+        spinner = (Spinner) findViewById(R.id.spinner_roomList);
+        adapter = ArrayAdapter.createFromResource(this, R.array.roomList, android.R.layout.simple_spinner_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+        spinner = (Spinner) findViewById(R.id.spinner_memberList);
+        adapter = ArrayAdapter.createFromResource(this, R.array.memberlist, android.R.layout.simple_spinner_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+        /*
+
+        //RoomList
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        spinner.setPrompt("이용 세미나실 선택");
+
+        adspin = ArrayAdapter.createFromResource(this, R.array.romList, android.R.layout.simple_spinner_item);
+
+        adspin.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adspin);
+        spinner.setOnItemSelectedListener(new SelectingItem() {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(ReservationFormActivity.this,
+                        adspin.getItem(position) + "을 선택 했습니다.", 1).show();
+            }
+
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        */
+
+        check_in_date = (EditText) findViewById(R.id.check_in_date);
+        check_in_time = (EditText) findViewById(R.id.check_in_time);
+        check_out_time = (EditText) findViewById(R.id.check_out_time);
+        useTime = (EditText) findViewById(R.id.useTime);
+        //roomNumber = (EditText) findViewById(R.id.roomNumber);
+        reason = (EditText) findViewById(R.id.reason);
+       // otherMembers = (EditText) findViewById(R.id.otherMembers);
+        button_makereservation = (Button) findViewById(R.id.button_makereservation);
+
+        //예약버튼 누르면 예약하기 함수 호출
         button_makereservation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 makereservation();
             }
-        }); //예약하기 함수 호출
+        });
+
+
 
 
         GregorianCalendar calendar = new GregorianCalendar();
@@ -103,19 +154,12 @@ public class ReservationFormActivity extends AppCompatActivity {
         }
     };
 
-
-
-
+/*
     public void onClickedMakeReservation(View v){
-        Toast.makeText(getApplicationContext(), "예약하기버튼눌림", Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), "예약버튼 눌림", Toast.LENGTH_LONG).show();
         finish();
     }
-
-
-
-
-
-
+*/
 
 
 
@@ -143,6 +187,7 @@ public class ReservationFormActivity extends AppCompatActivity {
     }
 
     //예약하기 함수
+    //서버로 예약 신청 정보 전송 : 짜야함
     public void makereservation(){ //예약하기 함수
         String id = check_in_date.getText().toString();
         String password = check_in_time.getText().toString();
@@ -152,5 +197,15 @@ public class ReservationFormActivity extends AppCompatActivity {
 
     }
 
+    //Spinner
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        TextView myText = (TextView) view;
+        Toast.makeText(ReservationFormActivity.this, "Room "+myText.getText()+" selected", Toast.LENGTH_SHORT).show();
+    }
 
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
